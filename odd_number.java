cf619b074edb7e5e3ed50cd4856d104717562248
Joe Joseph <joejoseph2028@cs.sjcetpalai.ac.in>
2025-07-23 10:12:20 +0530

    Add files via upload

---
+++ odd_number.java
@@ -0,0 +1,15 @@
+
+
+public class odd_number {
+	public static void main(String[] args) {
+		for(int i=1; i<=50; i++) {
+			if(i%2==0) {
+				continue;
+				
+			} 
+			System.out.print("\t");
+			System.out.print(i);
+		}
+	}
+
+}
